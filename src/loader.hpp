050d2c819f8a40a0d9342f1c8c1e1b0cbda9d9ce
Adrian Shvay <adrian.shvay@intel.com>
2017-06-28 14:26:33 +0200

    correst a comment

--- src/loader.hpp
+++ src/loader.hpp
@@ -187,7 +187,7 @@ private:
     nlohmann::json                      m_current_config;
     std::shared_ptr<web_app>            m_debug_web_app;
     
-    //Shows, how bigger should be batch size than CPU thread count to not use additional object to better fill thread pool 
+    // Shows how bigger should be batch size than CPU thread count to not use extended pipeline which increase input size for decoder
     const float                         m_increase_input_size_coefficient = 1.5; 
     // How many times we should increase input data size for decoder
     const int                           m_input_multiplier = 8;
