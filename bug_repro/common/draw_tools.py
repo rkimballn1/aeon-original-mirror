720a006add258aad2e184af85a0c454cab0a01d7
Piotr Majchrzak <piotr.majchrzak@intel.com>
2017-10-05 12:27:01 +0200

    Setup for reproduction 3 bugs in augmentation of aeon:
    1. Settings for resize have no effect
    2. No location settings generate a wrong error
    3. Rotation should not work with location but works

---
+++ bug_repro/common/draw_tools.py
@@ -0,0 +1,38 @@
+import numpy as np
+import os
+from PIL import Image as PILImage, ImageDraw
+
+cnt = 1
+
+
+def ensure_dir_exists(_dir):
+    directory = os.path.dirname(_dir)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+
+def draw_images(p_images, _prestring='aeon-output', _directory='output/'):
+    global cnt
+
+    for image, boxcount, gt_box_arr in zip(p_images[5][1], p_images[2][1], p_images[1][1]):
+        img = PILImage.fromarray(np.dstack(image)[:, :, ::-1])
+        draw = ImageDraw.Draw(img)
+        for k in range(boxcount):
+            gt_rectangle = [
+                gt_box_arr[k][0] * (image.shape[2]-1),
+                gt_box_arr[k][1] * (image.shape[1]-1),
+                gt_box_arr[k][2] * (image.shape[2]-1),
+                gt_box_arr[k][3] * (image.shape[1]-1)
+            ]
+            draw.rectangle(gt_rectangle)
+        ensure_dir_exists(_directory)
+        img.save('%s%s_%03d.png' % (_directory, _prestring, cnt))
+        cnt += 1
+
+
+def draw_image(_image, _file_name='aeon-output', _directory='output/'):
+    img = PILImage.fromarray(np.dstack(_image)[:, :, ::-1])
+    ImageDraw.Draw(img)
+    ensure_dir_exists(_directory)
+    img.save('%s%s.png' % (_directory, _file_name))
+
---
+++ bug_repro/draw_resized_images.py
@@ -0,0 +1,59 @@
+import os
+
+from aeon import DataLoader
+import common.draw_tools as dt
+
+
+def draw_augmented(aeon_config, output_dir):
+    image_set = DataLoader(aeon_config)
+    images = next(image_set)
+    dt.draw_images(images, _directory=output_dir)
+
+
+def draw_single_augmented(aeon_config, file_name, output_dir):
+    image_set = DataLoader(aeon_config)
+    images = next(image_set)
+    dt.draw_image(images[5][1][0], _file_name=file_name, _directory=output_dir)
+
+if __name__ == '__main__':
+
+    batch_size = 32
+    image_height = 300
+    image_width = 300
+
+    test_file_dir = os.path.dirname(os.path.realpath(__file__))
+    test_dir = test_file_dir + '/sample_images/'
+    output_dir = test_file_dir + '/resized/'
+    manifest_filename = 'manifest_four_figures_wsta.tsv'
+
+    image_config = {
+        'type': 'image',
+        'height': image_height,
+        'width': image_width
+    }
+
+    localization_config = {
+        'type': 'localization_ssd',
+        'height': image_height,
+        'width': image_width,
+        'class_names': ['arrow', 'square', 'triangle', 'wheel'],
+    }
+
+    augmentation_config = {
+        'type': 'image',
+        #'angle': [1, 45],
+        'scale': [0.1, 1.0],
+        'do_area_scale': True,
+        'crop_enable': False
+    }
+
+    aeon_config = {
+        'batch_size': batch_size,
+        'manifest_root': test_dir,
+        'manifest_filename': test_dir+manifest_filename,
+        'etl': [localization_config, image_config],  # localization_config must be first.
+       # 'etl': [image_config],
+        'augmentation': [augmentation_config]
+    }
+    draw_augmented(aeon_config, output_dir)
+    #draw_single_augmented(aeon_config, 'resized%03d', output_dir)
