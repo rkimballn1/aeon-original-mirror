28aab881e2d9b389ff7335596ca074b11c8e8d99
Tomasz Patejko <tomasz.patejko@intel.com>
2017-10-18 05:13:21 -0400

    Example of a python plugin run from aeon

---
+++ src/rotate.py
@@ -0,0 +1,14 @@
+import numpy as np
+import cv2
+
+def execute(mat, angle):
+    rows, cols = mat.shape
+
+    rotation_matrix = cv2.getRotationMatrix2D((cols / 2, rows / 2), angle, 1.0)
+    dst = cv2.warpAffine(mat, rotation_matrix, (cols, rows))
+
+    return dst
+
+#img = cv2.imread("dickbutt.jpg", 0)
+#new = execute(img, 45)
+#cv2.imwrite("dickbutt2.jpg", new)
