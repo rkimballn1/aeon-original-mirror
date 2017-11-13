7b2d882da42d3d6dd44a4cdb953d7669e78cf6cf
Sylwester Fraczek <sylwester.fraczek@intel.com>
2017-11-13 19:10:55 +0100

    commented out time consuming prints from py scripts

--- src/flip.py
+++ src/flip.py
@@ -5,14 +5,14 @@ import cv2
 
 class plugin(Plugin):
     def __init__(self, param_string):
-        print("# flip constructor called with "+param_string)
+#        print("# flip constructor called with "+param_string)
         pass
 
     def prepare(self):
-        print("# flip prepare called")
+#        print("# flip prepare called")
         pass
 
     def augment_image(self, mat):
-        print("# flip augment_image called with mat of size "+str(mat.size))
+#        print("# flip augment_image called with mat of size "+str(mat.size))
         dst = cv2.flip(mat, 1)
         return dst
