1d1c1485d17be797d76368d6f14a4b2881dec972
Sylwester Fraczek <sylwester.fraczek@intel.com>
2017-11-14 18:35:52 +0100

    plugins fixed again. fixup with last commit wit same name

--- src/rotate.py
+++ src/rotate.py
@@ -1,8 +1,7 @@
 import numpy as np
 import cv2
-from plugin import Plugin
 import json
-import random
+from plugin import Plugin
 
 
 class plugin(Plugin):
@@ -16,21 +15,15 @@ class plugin(Plugin):
         self.angle_max = params["angle"][1]
 
     def prepare(self):
-        angle = random.randint(self.angle_min, self.angle_max)
+        self.angle = np.random.uniform(self.angle_min, self.angle_max)
 
     def augment_image(self, mat):
-        if angle == 0:
+        if self.angle == 0:
             return mat
 
-        cols, rows = mat.shape()
-        rotation_matrix = cv2.getRotationMatrix2D((cols / 2, rows / 2), angle,
-                                                  1.0)
+        cols, rows, depth = mat.shape
+        rotation_matrix = cv2.getRotationMatrix2D((cols / 2, rows / 2),
+                                                  self.angle, 1.0)
         dst = cv2.warpAffine(mat, rotation_matrix, (cols, rows))
 
-        #print dst
         return dst
-
-
-#img = cv2.imread("dickbutt.jpg", 0)
-#new = execute(img, 45)
-#cv2.imwrite("dickbutt2.jpg", new)
