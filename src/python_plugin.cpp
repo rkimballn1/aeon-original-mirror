5ef516c66349acdbb31eba249fad4c59e00fcafc
Sylwester Fraczek <sylwester.fraczek@intel.com>
2017-11-14 12:02:04 +0100

    renamed python_plugin2 to python_plugin

--- src/python_plugin.cpp
+++ src/python_plugin.cpp
@@ -1,4 +1,4 @@
-#include "python_plugin2.hpp"
+#include "python_plugin.hpp"
 
 std::vector<std::shared_ptr<nervana::module>> nervana::plugin::loaded_modules;
 std::mutex                                    nervana::plugin::mtx;
