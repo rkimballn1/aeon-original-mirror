6c9ee5cd52b99ecbe72ff97f87df3650c6595580
Artur Wojcik <artur.wojcik@intel.com>
2018-03-05 15:39:00 +0100

    augmentation assistant
    Signed-off-by: Artur Wojcik <artur.wojcik@intel.com>

--- examples/augmentation_assistant/main.cpp
+++ examples/augmentation_assistant/main.cpp
@@ -19,9 +19,7 @@
 #include "aeon.hpp"
 #include "web_server.hpp"
 
-using namespace std;
-
-static string master_page = R"(
+constexpr char master_page[] = R"(
     <html>
     <head>
         <title>Aeon Debug</title>
@@ -59,122 +57,100 @@ static string master_page = R"(
     </html>
 )";
 
-static uint16_t port = 5000;
+constexpr uint16_t port = 5000;
 
-class augmentation_server
-{
+class augmentation_server {
 public:
-    augmentation_server()
-    {
-        page_request_handler fn =
-            bind(&augmentation_server::page_handler, this, placeholders::_1, placeholders::_2);
-        m_server.register_page_handler(fn);
-        m_server.start(port);
+  augmentation_server() {
+    m_server.register_page_handler(std::bind(&augmentation_server::page_handler, this, std::placeholders::_1, std::placeholders::_2));
+    m_server.start(port);
+  }
+
+  void page_handler(web::page& page, const std::string& url) {
+    if (url == "/") {
+      auto mc = std::bind(&augmentation_server::home_page, this, std::placeholders::_1);
+      page.master_page_string(master_page, "$content", mc);
     }
-
-    void page_handler(web::page& page, const std::string& url)
-    {
-        // ostream& out = page.output_stream();
-        if (url == "/")
-        {
-            auto mc = bind(&augmentation_server::home_page, this, placeholders::_1);
-            page.master_page_string(master_page, "$content", mc);
-        }
-        else if (url == "/exit")
-        {
-            // m_server.stop();
-        }
+    else if (url == "/exit") {
+      // m_server.stop();
     }
+  }
 
-    void wait_for_exit()
-    {
-        m_server.wait_for_exit();
-    }
-
-    void home_page(web::page& p)
-    {
-        time_t     t   = time(0);
-        struct tm* now = localtime(&t);
-        ostream&   out = p.output_stream();
-        nervana::image::config cfg;
-
-        out << "<span>Current time: " << asctime(now) << "</span>\n";
-
-        out << "<table class=\"table table-striped\">\n";
-        out << "  <thead>\n";
-        out << "    <th>Name</th>\n";
-        out << "    <th>Type</th>\n";
-        out << "    <th>Required</th>\n";
-        out << "    <th>Default</th>\n";
-        out << "  </thead>\n";
-        out << "  <tbody>\n";
-        for (auto x : cfg.get_config_list())                                                             \
-        {
-            out << "  <tr>\n";
-            out << "    <td>" << x->name() << "</td>\n";
-            out << "    <td>" << x->type()  << "</td>\n";
-            out << "    <td>" << (x->required() ? "REQUIRED" : "OPTIONAL")  << "</td>\n";
-            out << "    <td>" << (x->required() ? "" : x->get_default_value())  << "</td>\n";
-            out << "  </tr>\n";
-        }
-        out << "  </tbody>\n";
-        out << "</table>\n";
-
-
-
-        out << "<form class=\"form-horizontal\">\n";
-        for (auto x : cfg.get_config_list())                                                             \
-        {
-            out << "  <div class=\"form-group\">\n";
-            if (x->type() == "unsigned int" || x->type() == "float")
-            {
-                out << "    <label for=\"" << x->name() << "\" class=\"col-sm-2 control-label\">" << x->name() << "</label>\n";
-                out << "    <div class=\"col-sm-3\">\n";
-                out << "      <input type=\"number\" class=\"form-control\" id=\"" << x->name() << "\">\n";
-                out << "    </div>\n";
-            }
-            else if (x->type() == "std::string")
-            {
-                out << "    <label for=\"" << x->name() << "\" class=\"col-sm-2 control-label\">" << x->name() << "</label>\n";
-                out << "    <div class=\"col-sm-3\">\n";
-                out << "      <input type=\"text\" class=\"form-control\" id=\"" << x->name() << "\">\n";
-                out << "    </div>\n";
-            }
-            else if (x->type() == "bool")
-            {
-                out << "    <div class=\"col-sm-offset-2 col-sm-3\">\n";
-                out << "      <div class=\"checkbox\">\n";
-                out << "        <label>\n";
-                out << "          <input type=\"checkbox\" " << (x->get_default_value() == "0" ? "" : "checked") << "> " << x->name() << "\n";
-                out << "        </label>\n";
-                out << "      </div>\n";
-                out << "    </div>\n";
-            }
-            else if (x->type().find("distribution") != string::npos)
-            {
-                out << "    <label for=\"" << x->name() << "\" class=\"col-sm-2 control-label\">" << x->name() << "</label>\n";
-                out << "    <div class=\"col-sm-3\">\n";
-                out << "      <input type=\"text\" class=\"form-control\" id=\"" << x->name() << "\">\n";
-                out << "    </div>\n";
-            }
-            else
-            {
-                out << "<!-- " << x->type() << " -->\n";
-            }
-            out << "  </div>\n";
-        }
-        out << "</form>\n";
-    }
+  int wait_for_exit() {
+    m_server.wait_for_exit();
+    return 0;
+  }
 
 private:
-    web::server m_server;
-    vector<int> m_elements_size_list = {1024, 32};
+  web::server m_server;
+  std::vector<int> m_elements_size_list = { 1024, 32 };
+
+  void home_page(web::page& p) {
+    std::ostream& out = p.output_stream();
+    nervana::image::config cfg;
+
+    auto t{ std::chrono::system_clock::to_time_t(std::chrono::system_clock::now()) };
+    out << "<span>Current time: " << std::put_time(std::localtime(&t), "%Y-%m-%d %X") << "</span>\n";
+
+    out << "<table class=\"table table-striped\">\n";
+    out << "  <thead>\n";
+    out << "    <th>Name</th>\n";
+    out << "    <th>Type</th>\n";
+    out << "    <th>Required</th>\n";
+    out << "    <th>Default</th>\n";
+    out << "  </thead>\n";
+    out << "  <tbody>\n";
+    for (auto& x : cfg.get_config_list()) {
+      out << "  <tr>\n";
+      out << "    <td>" << x->name() << "</td>\n";
+      out << "    <td>" << x->type() << "</td>\n";
+      out << "    <td>" << (x->required() ? "REQUIRED" : "OPTIONAL") << "</td>\n";
+      out << "    <td>" << (x->required() ? "" : x->get_default_value()) << "</td>\n";
+      out << "  </tr>\n";
+    }
+    out << "  </tbody>\n";
+    out << "</table>\n";
+
+
+    out << R"(<form class="form-horizontal">\n)";
+    for (auto& x : cfg.get_config_list()) {
+      out << R"(  <div class="form-group">\n)";
+      if (x->type() == "unsigned int" || x->type() == "float") {
+        out << "    <label for=\"" << x->name() << R"(" class="col-sm-2 control-label">)" << x->name() << "</label>\n";
+        out << "    <div class=\"col-sm-3\">\n";
+        out << R"(      <input type="number" class="form-control" id=")" << x->name() << "\">\n";
+        out << "    </div>\n";
+            } 
+      else if (x->type() == "std::string") {
+        out << R"(    <label for=")" << x->name() << R"(" class="col-sm-2 control-label">)" << x->name() << "</label>\n";
+        out << R"(    <div class="col-sm-3">\n)";
+        out << R"(      <input type="text" class="form-control" id=")" << x->name() << R"(">\n)";
+        out << "    </div>\n";
+      }
+      else if (x->type() == "bool") {
+        out << R"(    <div class="col-sm-offset-2 col-sm-3">\n"
+                "      <div class="checkbox">\n"
+                "        <label>\n"
+                "          <input type="checkbox" )" << (x->get_default_value() == "0" ? "" : "checked") << "> "
+            << x->name() << "\n        </label>\n      </div>\n    </div>\n";
+      }
+      else if (x->type().find("distribution") != std::string::npos) {
+        out << R"(    <label for=")" << x->name() << R"(" class="col-sm-2 control-label">)" << x->name() << "</label>\n";
+        out << R"(    <div class="col-sm-3">\n)";
+        out << R"(      <input type="text" class="form-control" id=")" << x->name() << "\">\n    </div>\n";
+      }
+      else {
+        out << "<!-- " << x->type() << " -->\n";
+      }
+      out << "  </div>\n";
+    }
+    out << "</form>\n";
+  }
 };
 
 int main(int argc, char** argv)
 {
+    augmentation_server server{};
     std::cout << "server started on port " << port << std::endl;
-    augmentation_server server;
-
-    server.wait_for_exit();
+    return server.wait_for_exit();
 }
