1fa03fd6a3436e07275ce4445ddf47c819ea6136
Artur Wojcik <artur.wojcik@intel.com>
2018-03-05 15:39:00 +0100

    fix etl_image klocwork issues
    Signed-off-by: Artur Wojcik <artur.wojcik@intel.com>

--- src/etl_image.hpp
+++ src/etl_image.hpp
@@ -25,160 +25,160 @@
 #include "util.hpp"
 #include "augment_image.hpp"
 
-namespace nervana
-{
-    namespace image
-    {
-        class config;
-        class decoded;
-
-        class extractor;
-        class transformer;
-        class loader;
-    }
-    namespace video
-    {
+namespace nervana {
+    namespace video {
         class config; // Forward decl for friending
     }
-
-    namespace multicrop
-    {
+    namespace multicrop {
         class config; // Forward decl for friending
     }
 }
 
-/**
- * \brief Configuration for image ETL
- *
- * An instantiation of this class controls the ETL of image data into the
- * target memory buffers from the source CPIO archives.
- */
-class nervana::image::config : public interface::config
-{
-    friend class video::config;
-    friend class multicrop::config;
+namespace nervana {
+  namespace image {
 
-public:
-    uint32_t    height;
-    uint32_t    width;
-    std::string output_type{"uint8_t"};
+    /**
+     * \brief Configuration for image ETL
+     *
+     * An instantiation of this class controls the ETL of image data into the
+     * target memory buffers from the source CPIO archives.
+     */
+    class config : public interface::config {
+      friend class nervana::video::config;
+      friend class nervana::multicrop::config;
 
-    bool     channel_major = true;
-    uint32_t channels      = 3;
+    public:
+      uint32_t height{ 0 };
+      uint32_t width{ 0 };
+      std::string output_type{ "uint8_t" };
 
-    std::string name;
+      bool channel_major{ true };
+      uint32_t channels{ 3 };
 
-    config(nlohmann::json js);
+      std::string name{};
 
-    const std::vector<std::shared_ptr<interface::config_info_interface>>& get_config_list()
-    {
-        return config_list;
-    }
+      explicit config(const nlohmann::json& js);
 
-    config() {}
-private:
-    std::vector<std::shared_ptr<interface::config_info_interface>> config_list = {
+      const std::vector<std::shared_ptr<interface::config_info_interface>>& get_config_list() {
+          return config_list;
+      }
+
+      config() = default;
+
+    private:
+      std::vector<std::shared_ptr<interface::config_info_interface>> config_list = {
         ADD_SCALAR(height, mode::REQUIRED),
         ADD_SCALAR(width, mode::REQUIRED),
         ADD_SCALAR(name, mode::OPTIONAL),
         ADD_SCALAR(channel_major, mode::OPTIONAL),
-        ADD_SCALAR(channels, mode::OPTIONAL, [](uint32_t v) { return v == 1 || v == 3; }),
+        ADD_SCALAR(channels, mode::OPTIONAL, [](uint32_t v) {
+          return v == 1 || v == 3;
+        }),
         ADD_SCALAR(output_type, mode::OPTIONAL, [](const std::string& v) {
-            return output_type::is_valid_type(v);
-        })};
+          return output_type::is_valid_type(v);
+        }) };
 
-    void validate();
+      void validate();
 
-    bool flip_enable = false;
-    bool center      = true;
-};
+      bool flip_enable{ false };
+      bool center{ true };
+    };
 
 // ===============================================================================================
 // Decoded
 // ===============================================================================================
 
-class nervana::image::decoded : public interface::decoded_image
-{
-public:
-    decoded() {}
-    decoded(cv::Mat img) { _images.push_back(img); }
-    bool add(cv::Mat img)
-    {
-        _images.push_back(img);
-        return all_images_are_same_size();
-    }
-    bool add(const std::vector<cv::Mat>& images)
-    {
-        for (auto mat : images)
-        {
-            _images.push_back(mat);
-        }
-        return all_images_are_same_size();
-    }
-    virtual ~decoded() override {}
-    cv::Mat& get_image(int index) { return _images[index]; }
-    cv::Size2i             get_image_size() const { return _images[0].size(); }
-    int                    get_image_channels() const { return _images[0].channels(); }
-    size_t                 get_image_count() const { return _images.size(); }
-    size_t                 get_size() const
-    {
-        return get_image_size().area() * get_image_channels() * get_image_count();
-    }
-    cv::Size2i image_size() const override { return _images[0].size(); }
-protected:
-    bool all_images_are_same_size()
-    {
-        for (int i = 1; i < _images.size(); i++)
-        {
-            if (_images[0].size() != _images[i].size())
-                return false;
-        }
-        return true;
-    }
-    std::vector<cv::Mat> _images;
-};
+    class decoded : public interface::decoded_image {
+    public:
+      decoded() {
+      }
+      decoded(cv::Mat img) {
+          _images.push_back(img);
+      }
+      bool add(cv::Mat img) {
+          _images.push_back(img);
+          return all_images_are_same_size();
+      }
+      bool add(const std::vector<cv::Mat>& images) {
+          for (auto mat : images) {
+              _images.push_back(mat);
+          }
+          return all_images_are_same_size();
+      }
 
-class nervana::image::extractor : public interface::extractor<image::decoded>
-{
-public:
-    extractor(const image::config&);
-    ~extractor() {}
-    virtual std::shared_ptr<image::decoded> extract(const void*, size_t) const override;
+      virtual ~decoded() override = default;
 
-    int get_channel_count() { return _color_mode == CV_LOAD_IMAGE_COLOR ? 3 : 1; }
-private:
-    int _pixel_type;
-    int _color_mode;
-};
+      cv::Mat& get_image(int index) {
+          return _images[index];
+      }
+      cv::Size2i get_image_size() const {
+          return _images[0].size();
+      }
+      int get_image_channels() const {
+          return _images[0].channels();
+      }
+      size_t get_image_count() const {
+          return _images.size();
+      }
+      size_t get_size() const {
+          return get_image_size().area() * get_image_channels() * get_image_count();
+      }
+      cv::Size2i image_size() const override {
+          return _images[0].size();
+      }
+    protected:
+      bool all_images_are_same_size() {
+          for (int i = 1; i < _images.size(); i++) {
+              if (_images[0].size() != _images[i].size())
+                  return false;
+          }
+          return true;
+      }
+      std::vector<cv::Mat> _images;
+    };
 
-class nervana::image::transformer
-    : public interface::transformer<image::decoded, augment::image::params>
-{
-public:
-    transformer(const image::config&);
-    ~transformer() {}
-    virtual std::shared_ptr<image::decoded>
-        transform(std::shared_ptr<augment::image::params>,
-                  std::shared_ptr<image::decoded>) const override;
+    class extractor : public interface::extractor<image::decoded> {
+    public:
+      explicit extractor(const image::config&);
+      std::shared_ptr<image::decoded> extract(const void*, size_t) const override;
 
-    cv::Mat transform_single_image(std::shared_ptr<augment::image::params>, cv::Mat&) const;
+      int get_channel_count() {
+          return _color_mode == CV_LOAD_IMAGE_COLOR ? 3 : 1;
+      }
+    private:
+      int _pixel_type;
+      int _color_mode;
+    };
 
-private:
-    image::photometric photo;
-};
+    class transformer : public interface::transformer<image::decoded, augment::image::params> {
+    public:
+      explicit transformer(const image::config&);
 
-class nervana::image::loader : public interface::loader<image::decoded>
-{
-public:
-    loader(const image::config& cfg, bool fixed_aspect_ratio);
-    ~loader() {}
-    virtual void load(const std::vector<void*>&, std::shared_ptr<image::decoded>) const override;
+      virtual std::shared_ptr<image::decoded>
+      transform(std::shared_ptr<augment::image::params>,
+                std::shared_ptr<image::decoded>) const override;
 
-private:
-    void split(cv::Mat&, char*);
+      cv::Mat transform_single_image(std::shared_ptr<augment::image::params>, cv::Mat&) const;
 
-    bool       m_channel_major;
-    bool       m_fixed_aspect_ratio;
-    shape_type m_stype;
-    uint32_t   m_channels;
-};
+    private:
+      image::photometric photo;
+    };
+
+    class loader : public interface::loader<image::decoded> {
+    public:
+      loader(const image::config& cfg, bool fixed_aspect_ratio);
+      ~loader() {
+      }
+      virtual void load(const std::vector<void*>&, std::shared_ptr<image::decoded>) const override;
+
+    private:
+      void split(cv::Mat&, char*);
+
+      bool m_channel_major;
+      bool m_fixed_aspect_ratio;
+      shape_type m_stype;
+      uint32_t m_channels;
+    };
+
+  }
+}
